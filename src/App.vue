a214818429ef5b7807ab7aa9b704a5e4bbb70e2b
wuchao288 <371126627@qq.com>
2024-08-31 17:32:15 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,105 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+
+import { App, Rect } from 'leafer-ui'
+import '@leafer-in/editor' // 导入图形编辑器插件
+import { Arrow } from '@leafer-in/arrow'
+
+import { EditTool, registerEditTool } from '@leafer-in/editor'
+
+registerEditTool();
+ // 1. 注册编辑工具
+// export class CustomEditTool extends EditTool {
+//     public get tag() { return 'CustomEditTool' } // 2. 定义全局唯一的 tag 名称
+// }
+
+import { HTMLText } from '@leafer-in/html'
+import { onMounted } from 'vue'
+
+
+onMounted(function(){
+  const app = new App({
+    view: 'mainCanvas',
+    editor: {} // 会自动创建 editor实例、tree层、sky层
+})
+
+
+app.tree.add(Rect.one({zIndex:0, editable: true, fill: '#FEB027',
+ cornerRadius: [20, 0, 0, 20] }, 100, 100))
+
+app.tree.add(Rect.one({zIndex:1, editable: true, fill:
+     '#FFE04B', cornerRadius: [0, 20, 20, 0] }, 300, 100))
+
+
+     const arrow = new Arrow({
+        y: 50,
+        height: 20,
+        startArrow:'mark',
+        endArrow:'mark',
+        editable: true,
+        strokeWidth: 2,
+        stroke: 'rgb(50,205,121)'
+    })
+    app.tree.add(arrow);
+
+
+    const text = new HTMLText({
+        x: 100,
+        y: 200,
+        editable: true,
+        strokeWidth: 2,
+        stroke: 'rgb(0,0,0)',
+        fill: 'rgb(50,205,121)',
+        fontSize: 30,
+        fontFamily: 'Microsoft YaHei',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        text: '32cm',
+    })
+
+
+    
+
+    app.tree.add(text);
+
+});
+
+
 </script>
 
+
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+
+  <Header msg="Vite + Vue" />
+
+  <div id="main">
+    
+    <div id="mainCanvas"> 
+
+    </div>
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+   #main{
+     position: absolute;
+     top: 81px;
+     left: 0px;
+     right: 0px;
+     bottom: 0px;
+     text-align: center;
+     display: flex;
+      align-content: center;
+      justify-content: center;
+   }
+#mainCanvas{
+
+  height: 600px;
+  width: 800px;
+  box-shadow: 0 0 .4rem #0000004d;
+    filter: drop-shadow(0 0 1.2rem rgba(255, 255, 255, .6));
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+
+
+</script>
+
+<template>
+ <div class="header">
+     <div class="action">
+         <ul>
+             <li><button>Image</button></li>
+             <li><button>Text</button></li>
+             <li><button>Ellipse</button></li>
+             <li><button>Line</button></li>
+             <li><button>Arrow</button></li>
+             <li><button>Save As</button></li>
+         </ul>
+     </div>
+ </div>
+</template>
+
+<style scoped>
+.header{
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    height: 60px;
+    right: 0px;
+    padding: 0px;
+    box-shadow: 0 0 .4rem #0000004d;
+    filter: drop-shadow(0 0 1.2rem rgba(255, 255, 255, .6));
+}
+.action{
+  padding: 0px;
+  margin: 0px;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  height: 100%;
+}
+.action ul{
+   display: flex;
+   flex-direction: row;
+   padding: 0px;
+   margin: 0px;
+}
+.action ul li{
+  margin-right: 20px;
+}
+</style>
